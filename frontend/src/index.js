03dfadecb5e837c0d6210a9208ca04f227c8939d
felxx <igorfelipemonteirocontato@gmail.com>
2025-08-04 22:30:50 -0300

    feat(frontend); import prime react dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+import 'primereact/resources/themes/arya-orange/theme.css'; 
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
