b7f9cc1379845f0fd3022797c9a919d209627bd3
felxx <igorfelipemonteirocontato@gmail.com>
2025-08-18 09:20:58 -0300

    feat(frontend): implements authentication logic on frontend

---
+++ frontend/src/services/auth/authService.js
@@ -0,0 +1,20 @@
+import api from '../api';
+
+const login = async (email, password) => {
+  const response = await api.post('/auth/login', { email, password });
+  if (response.data.token) {
+    localStorage.setItem('token', response.data.token);
+  }
+  return response.data;
+};
+
+const logout = () => {
+  localStorage.removeItem('token');
+};
+
+const authService = {
+  login,
+  logout,
+};
+
+export default authService;
